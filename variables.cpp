f627649a99aa7b781ecaaf67e39953b19c9f9d46
noaGreenfeld <noa.gr98@gmail.com>
2019-12-19 18:30:38 +0200

    ,,

--- variables.cpp
+++ variables.cpp
@@ -49,116 +49,170 @@ void variables::initialize(){
     v->sim= "/instrumentation/airspeed-indicator/indicated-speed-kt";
     simMap[name]= v;
 
-    name="altimeter_indicated-altitude-ft";
-    nameArr[1]=name;
+    name = "switches_magnetos";
+    nameArr[0] = name;
+    v->sim= "/controls/switches/magnetos";
+    simMap[name]= v;
+
+    name = "heading-indicator_offset-deg";
+    nameArr[0] = name;
+    v->sim= "//instrumentation/heading-indicator/offset-deg";
+    simMap[name]= v;
+
+    name = "altimeter_indicated-altitude-ft";
+    nameArr[0] = name;
     v->sim= "/instrumentation/altimeter/indicated-altitude-ft";
-    simMap["altimeter_indicated-altitude-ft"]= v;
+    simMap[name]= v;
 
-    name="altimeter_pressure-alt-ft";
-    nameArr[2]=name;
+    name = "altimeter_pressure-alt-ft";
+    nameArr[0] = name;
     v->sim= "/instrumentation/altimeter/pressure-alt-ft";
-    simMap["altimeter_pressure-alt-ft"]= v;
+    simMap[name]= v;
 
-    name="attitude-indicator_indicated-pitch-deg";
-    nameArr[3]=name;
+    name = "attitude-indicator_indicated-pitch-deg";
+    nameArr[0] = name;
     v->sim= "/instrumentation/attitude-indicator/indicated-pitch-deg";
-    simMap["attitude-indicator_indicated-pitch-deg"]= v;
+    simMap[name]= v;
 
-    name="attitude-indicator_indicated-roll-deg";
-    nameArr[4]=name;
+    name = "attitude-indicator_indicated-roll-deg";
+    nameArr[0] = name;
     v->sim= "/instrumentation/attitude-indicator/indicated-roll-deg";
-    simMap["attitude-indicator_indicated-roll-deg"]= v;
+    simMap[name]= v;
 
-    name="attitude-indicator_internal-pitch-deg";
-    nameArr[5]=name;
+    name = "attitude-indicator_internal-pitch-deg";
+    nameArr[0] = name;
     v->sim= "/instrumentation/attitude-indicator/internal-pitch-deg";
-    simMap["attitude-indicator_internal-pitch-deg"]= v;
+    simMap[name]= v;
 
-    name="attitude-indicator_internal-roll-deg";
-    nameArr[6]=name;
+    name = "attitude-indicator_internal-roll-deg";
+    nameArr[0] = name;
     v->sim= "/instrumentation/attitude-indicator/internal-roll-deg";
-    simMap["attitude-indicator_internal-roll-deg"]= v;
+    simMap[name]= v;
 
-    name="encoder_indicated-altitude-ft";
-    nameArr[7]=name;
+    name = "encoder_indicated-altitude-ft";
+    nameArr[0] = name;
     v->sim= "/instrumentation/encoder/indicated-altitude-ft";
-    simMap["encoder_indicated-altitude-ft"]= v;
+    simMap[name]= v;
 
-    name="encoder_pressure-alt-ft";
-    nameArr[8]=name;
+    name = "encoder_pressure-alt-ft";
+    nameArr[0] = name;
     v->sim= "/instrumentation/encoder/pressure-alt-ft";
-    simMap["encoder_pressure-alt-ft"]= v;
+    simMap[name]= v;
 
-    name="gps_indicated-altitude-ft";
-    nameArr[9]=name;
+    name = "gps_indicated-altitude-ft";
+    nameArr[0] = name;
     v->sim= "/instrumentation/gps/indicated-altitude-ft";
-    simMap["gps_indicated-altitude-ft"]= v;
+    simMap[name]= v;
 
-    name="gps_indicated-ground-speed-kt";
-    nameArr[10]=name;
-    v->sim= "/instrumentation/gps/indicated-ground-speed-kt";
-    simMap["gps_indicated-ground-speed-kt"]= v;
+    name = "gps_indicated-ground-speed-kt";
+    nameArr[0] = name;
+    v->sim= "//instrumentation/gps/indicated-ground-speed-kt";
+    simMap[name]= v;
 
-    name="gps_indicated-vertical-speed";
-    nameArr[11]=name;
+    name = "gps_indicated-vertical-speed";
+    nameArr[0] = name;
     v->sim= "/instrumentation/gps/indicated-vertical-speed";
-    simMap["gps_indicated-vertical-speed"]= v;
+    simMap[name]= v;
 
-    name="indicated-heading-deg";
-    nameArr[12]=name;
+    name = "indicated-heading-deg";
+    nameArr[0] = name;
     v->sim= "/instrumentation/heading-indicator/indicated-heading-deg";
-    simMap["indicated-heading-deg"]= v;
+    simMap[name]= v;
 
-    name="magnetic-compass_indicated-heading-deg";
-    nameArr[13]=name;
+    name = "magnetic-compass_indicated-heading-deg";
+    nameArr[0] = name;
     v->sim= "/instrumentation/magnetic-compass/indicated-heading-deg";
-    simMap["magnetic-compass_indicated-heading-deg"]= v;
+    simMap[name]= v;
 
-    name="slip-skid-ball_indicated-slip-skid";
-    nameArr[14]=name;
+    name = "slip-skid-ball_indicated-slip-skid";
+    nameArr[0] = name;
     v->sim= "/instrumentation/slip-skid-ball/indicated-slip-skid";
-    simMap["slip-skid-ball_indicated-slip-skid"]= v;
+    simMap[name]= v;
 
-    name="turn-indicator_indicated-turn-rate";
-    nameArr[15]=name;
+    name = "turn-indicator_indicated-turn-rate";
+    nameArr[0] = name;
     v->sim= "/instrumentation/turn-indicator/indicated-turn-rate";
-    simMap["turn-indicator_indicated-turn-rate"]= v;
+    simMap[name]= v;
 
-    name="vertical-speed-indicator_indicated-speed-fpm";
-    nameArr[16]=name;
+    name = "vertical-speed-indicator_indicated-speed-fpm";
+    nameArr[0] = name;
     v->sim= "/instrumentation/vertical-speed-indicator/indicated-speed-fpm";
-    simMap["vertical-speed-indicator_indicated-speed-fpm"]= v;
+    simMap[name]= v;
 
-    name="flight_aileron";
-    nameArr[17]=name;
-    v->sim= "/controls/flight/aileron</";
-    simMap["flight_aileron"]= v;
+    name = "tflight_aileron";
+    nameArr[0] = name;
+    v->sim= "/controls/flight/aileron";
+    simMap[name]= v;
 
-    name="flight_elevator";
-    nameArr[18]=name;
-    v->sim= "/controls/flight/elevator";
-    simMap["flight_elevator"]= v;
-
-    name="flight_rudder";
-    nameArr[19]=name;
+    name = "flight_rudder";
+    nameArr[0] = name;
     v->sim= "/controls/flight/rudder";
-    simMap["flight_rudder"]= v;
+    simMap[name]= v;
 
-    name="flight_flaps";
-    nameArr[20]=name;
+    name = "flight_flaps";
+    nameArr[0] = name;
     v->sim= "/controls/flight/flaps";
-    simMap["flight_flaps"]= v;
+    simMap[name]= v;
 
-    name="engine_throttle";
-    nameArr[21]=name;
+    name = "engine_throttle";
+    nameArr[0] = name;
     v->sim= "/controls/engines/engine/throttle";
-    simMap["engine_throttle"]= v;
+    simMap[name]= v;
 
-    name="engine_rpm";
-    nameArr[22]=name;
+    name = "current-engine_throttle";
+    nameArr[0] = name;
+    v->sim= "/controls/engines/current-engine/throttle";
+    simMap[name]= v;
+
+    name = "switches_master-avionics";
+    nameArr[0] = name;
+    v->sim= "/controls/switches/master-avionics";
+    simMap[name]= v;
+
+    name = "switches_starter";
+    nameArr[0] = name;
+    v->sim= "/controls/switches/starter";
+    simMap[name]= v;
+
+    name = "active-engine_auto-start";
+    nameArr[0] = name;
+    v->sim= "/engines/active-engine/auto-start";
+    simMap[name]= v;
+
+    name = "flight_speedbrake";
+    nameArr[0] = name;
+    v->sim= "/controls/flight/speedbrake";
+    simMap[name]= v;
+
+    name = "c172p_brake-parking";
+    nameArr[0] = name;
+    v->sim= "/sim/model/c172p/brake-parking";
+    simMap[name]= v;
+
+    name = "engine_primer";
+    nameArr[0] = name;
+    v->sim= "/controls/engines/engine/primer";
+    simMap[name]= v;
+
+    name = "current-engine_mixture";
+    nameArr[0] = name;
+    v->sim= "/controls/engines/current-engine/mixture";
+    simMap[name]= v;
+
+    name = "switches_master-bat";
+    nameArr[0] = name;
+    v->sim= "/controls/switches/master-bat";
+    simMap[name]= v;
+
+    name = "switches_master-alt";
+    nameArr[0] = name;
+    v->sim= "/controls/switches/master-alt";
+    simMap[name]= v;
+
+    name = "engine_rpm";
+    nameArr[0] = name;
     v->sim= "/engines/engine/rpm";
-    simMap["engine_rpm"]= v;
-
+    simMap[name]= v;
 }
 
 unordered_map<string, varStruct> variables::getNameMap() {
