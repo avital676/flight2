042c2dd967aa6641d48aac4072da6901910042aa
avital <avital676@gmail.com>
2019-12-23 19:35:26 +0200

    starting new changes

--- variables.cpp
+++ variables.cpp
@@ -43,79 +43,79 @@ varObj* variables::getVarFromSim(string v) {
 }
 
 void variables::initialize() {
-    varObj *v0 = new varObj;
+    //varObj *v0 = new varObj;
     string name;
     name = "airspeed-indicator_indicated-speed-kt";
     nameArr[0] = name;
-    v0->setSim("/instrumentation/airspeed-indicator/indicated-speed-kt");
-    simMap[name] = v0;
+    //v0->setSim("/instrumentation/airspeed-indicator/indicated-speed-kt");
+    //simMap[name] = v0;
 
-    varObj *v1 = new varObj;
+    //varObj *v1 = new varObj;
     name = "time_warp";
     nameArr[1] = name;
-    v1->setSim("/sim/time/warp");
-    simMap[name] = v1;
+    //v1->setSim("/sim/time/warp");
+    //simMap[name] = v1;
 
-    varObj *v2 = new varObj;
+    //varObj *v2 = new varObj;
     name = "switches_magnetos";
     nameArr[2] = name;
-    v2->setSim("/controls/switches/magnetos");
-    simMap[name] = v2;
+    //v2->setSim("/controls/switches/magnetos");
+    //simMap[name] = v2;
 
-    varObj *v3 = new varObj;
+    //varObj *v3 = new varObj;
     name = "heading-indicator_offset-deg";
     nameArr[3] = name;
-    v3->setSim("/instrumentation/heading-indicator/offset-deg");
-    simMap[name] = v3;
+    //v3->setSim("/instrumentation/heading-indicator/offset-deg");
+    //simMap[name] = v3;
 
-    varObj *v4 = new varObj;
+    //varObj *v4 = new varObj;
     name = "altimeter_indicated-altitude-ft";
     nameArr[4] = name;
-    v4->setSim("/instrumentation/altimeter/indicated-altitude-ft");
-    simMap[name] = v4;
+    //v4->setSim("/instrumentation/altimeter/indicated-altitude-ft");
+    //simMap[name] = v4;
 
-    varObj *v5 = new varObj;
+    //varObj *v5 = new varObj;
     name = "altimeter_pressure-alt-ft";
     nameArr[5] = name;
-    v5->setSim("/instrumentation/altimeter/pressure-alt-ft");
-    simMap[name] = v5;
+    //v5->setSim("/instrumentation/altimeter/pressure-alt-ft");
+    //simMap[name] = v5;
 
-    varObj *v6 = new varObj;
+    //varObj *v6 = new varObj;
     name = "attitude-indicator_indicated-pitch-deg";
     nameArr[6] = name;
-    v6->setSim("/instrumentation/attitude-indicator/indicated-pitch-deg");
-    simMap[name] = v6;
+    //v6->setSim("/instrumentation/attitude-indicator/indicated-pitch-deg");
+    //simMap[name] = v6;
 
-    varObj *v7 = new varObj;
+    //varObj *v7 = new varObj;
     name = "attitude-indicator_indicated-roll-deg";
     nameArr[7] = name;
-    v7->setSim("/instrumentation/attitude-indicator/indicated-roll-deg");
-    simMap[name] = v7;
+    //v7->setSim("/instrumentation/attitude-indicator/indicated-roll-deg");
+    //simMap[name] = v7;
 
-    varObj *v8 = new varObj;
+    //varObj *v8 = new varObj;
     name = "attitude-indicator_internal-pitch-deg";
     nameArr[8] = name;
-    v8->setSim("/instrumentation/attitude-indicator/internal-pitch-deg");
-    simMap[name] = v8;
+    //v8->setSim("/instrumentation/attitude-indicator/internal-pitch-deg");
+    //simMap[name] = v8;
 
-    varObj *v9 = new varObj;
+    //varObj *v9 = new varObj;
     name = "attitude-indicator_internal-roll-deg";
     nameArr[9] = name;
-    v9->setSim("/instrumentation/attitude-indicator/internal-roll-deg");
-    simMap[name] = v9;
+    //v9->setSim("/instrumentation/attitude-indicator/internal-roll-deg");
+    //simMap[name] = v9;
 
-    varObj *v10 = new varObj;
+    //varObj *v10 = new varObj;
     name = "encoder_indicated-altitude-ft";
     nameArr[10] = name;
-    v10->setSim("/instrumentation/encoder/indicated-altitude-ft");
-    simMap[name] = v10;
+    //v10->setSim("/instrumentation/encoder/indicated-altitude-ft");
+    //simMap[name] = v10;
 
 
-    varObj *v11 = new varObj;
+    //varObj *v11 = new varObj;
     name = "encoder_pressure-alt-ft";
     nameArr[11] = name;
-    v11->setSim("/instrumentation/encoder/pressure-alt-ft");
-    simMap[name] = v11;
+    //v11->setSim("/instrumentation/encoder/pressure-alt-ft");
+    //simMap[name] = v11;
 
     varObj *v12 = new varObj;
     name = "gps_indicated-altitude-ft";
