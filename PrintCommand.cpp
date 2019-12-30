2e8e991e8b12e04e38c75693c58a72c179e6cc7d
noaGreenfeld <58787259+noaGreenfeld@users.noreply.github.com>
2019-12-30 21:00:30 +0200

    ready to submit

--- PrintCommand.cpp
+++ PrintCommand.cpp
@@ -7,13 +7,13 @@
 // Execute print command:
 int PrintCommand::execute(int i, vector<string> v) {
     unordered_map<string, varObj*> m =variables::getInstance()->getNameMap();
-    if (m.find(v[i + 1]) != m.end()) {
-        float value = m[v[i+1]]->getVal();
-        cout<<value<<endl;
-    } else {
-        //cut the " "
+    if (v[i+1][0]=='"'){
         string toPrint = v[i+1].substr(1, v[i+1].length()-2);
         cout << toPrint << endl;
+    }
+    else{
+        float value = express(v[i+1]);
+        cout<<value<<endl;
     }
     numOfPar = 1;
     return numOfPar + 1;
